bba413b3793bc12116cc334837d8dd7b42f86eaa
Ross Brandon <rosstafarian1@gmail.com>
2021-01-16 17:31:19 -0600

    Remove /api from route (using subdomain api.spendy.dev)

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ dotenv.config()
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    app.setGlobalPrefix('api')
     app.enableCors()
     app.useGlobalPipes(
         new ValidationPipe({
