017b7063af8e52a4b54ba67db1d9297be736ac41
Ross Brandon <rosstafarian1@gmail.com>
2021-01-21 12:42:27 -0600

    Update redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+echo "-------------------------------"
+echo "Stage: Pull from github - Begin"
+echo "-------------------------------"
+
 # change to project directory - required for forever for some reason
 cd /home/bitnami/stack/projects/spendy-backend || exit
 
 git pull
 
+echo "-----------------------------"
+echo "Stage: Pull from github - End"
+echo "-----------------------------"
+
+echo "-----------------------"
+echo "Stage: Redeploy - Begin"
+echo "-----------------------"
+
+echo "Previous process:"
+forever list | grep 'spendy-backend'
+
+echo "Restarting app server..."
 if forever list | grep 'spendy-backend'; then forever stop spendy-backend; fi
 forever -a --uid spendy-backend start /home/bitnami/stack/projects/spendy-backend/dist/main.js
+
+echo "New process:"
+forever list | grep 'spendy-backend'
+
+echo "---------------------"
+echo "Stage: Redeploy - End"
+echo "---------------------"
