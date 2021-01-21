b60624282f155f5f11031e215a51c19d5fd2a1bb
Ross Brandon <rosstafarian1@gmail.com>
2021-01-21 12:13:21 -0600

    Retrying with travis --pro

--- src/redeploy.sh
+++ src/redeploy.sh
@@ -3,5 +3,7 @@
 # change to project directory - required for forever for some reason
 cd /home/bitnami/stack/projects/spendy-backend || exit
 
+git pull
+
 if forever list | grep 'spendy-backend'; then forever stop spendy-backend; fi
 forever -a --uid spendy-backend start /home/bitnami/stack/projects/spendy-backend/dist/main.js
